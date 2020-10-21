ae327b3a07d9b86a303da30849c5132a8572678a
asdf <secret@kali>
2020-10-21 14:46:19 -0500

    toolbar icon

--- logo.sh
+++ logo.sh
@@ -5,7 +5,8 @@ set -euxo pipefail
 BOT=Twemoji_1f346.svg.png
 TOP=No_circle.svg.png
 
-for k in 24 48 96 192 ; do
+for k in 24 32 48 96 192 ; do
+  [[ ! -e icons/no-eggplant-${k}x${k}.png ]] || continue
   convert   -quality 100 -gravity center -resize ${k}x${k} -extent ${k}x${k} -transparent white $BOT  eggplant-${k}x${k}.png
   convert   -quality 100 -gravity center -resize ${k}x${k} -extent ${k}x${k} -transparent white $TOP no-circle-${k}x${k}.png 
   composite -quality 100 -gravity center no-circle-${k}x${k}.png eggplant-${k}x${k}.png      icons/no-eggplant-${k}x${k}.png
