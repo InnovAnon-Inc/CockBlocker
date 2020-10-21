3af3bfa07bd0ac8d07c982e5380ba87366ef50eb
asdf <secret@kali>
2020-10-21 18:07:25 -0500

    update

--- cock-blocker.js
+++ cock-blocker.js
@@ -12,19 +12,32 @@
 console.log ('cbt exec content.js');
 
 // guard to prevent re-running
-if (window.hasRun) { return; }
-window.hasRun = true;
+//if (window.hasRun) { return; }
+//window.hasRun = true;
 
-var imgs = document.images;
-for (var i = 0, iLen = imgs.length; i < iLen; i++) {
-  let img = imgs[i];
-  console.log ('cbt replacing image ' + img.src);
-  //let mat = cv.imread (img);
-  //cv.imshow ('canvasOutput', mat);
-  //mat.delete ();
-  // if cv_src is not dick pic: return
-  let repl = browser.extension.getURL ('replacement.png')
-  img.setAttribute ('src', repl);
+function replaceImages () {
+  console.log ('cbt replaceImages ()');
+  var imgs = document.images;
+  var repl = browser.extension.getURL ('replacement.png')
+  for (var i = 0, iLen = imgs.length; i < iLen; i++) {
+    let img = imgs[i];
+    console.log ('cbt checking image 1 ' + img.src);
+    if (img.src == repl) { continue; }
+    console.log ('cbt checking image 2');
+    let mat = cv.imread (img);
+    console.log ('opencv read img');
+    //cv.imshow ('canvasOutput', mat);
+    if (true) { // is a dick pic
+      console.log ('cbt dick pic detected');
+      img.setAttribute ('src', repl);
+      console.log ('cbt image replaced');
+    }
+    mat.delete ();
+    console.log ('opencv free mem');
+  }
 }
+replaceImages ();
+
+//browser.onStateChanged ()
 
 console.log ('cbt images replaced');
