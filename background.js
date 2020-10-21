fc6891b7b593dc47365b3f10950f51bb34f2e2c4
asdf <secret@kali>
2020-10-21 14:02:31 -0500

    context menus stopped working before initial commit. everything is fucked

--- background.js
+++ background.js
@@ -2,14 +2,16 @@
 // adds/removes images to/from model
 
 browser.contextMenus.create ({
-    id: "register-dick-pic",
-    title: "Add Dick Pic to Registry",
-    contexts: ["image"],
+    id      : "register-dick-pic",
+    title   : "Add Dick Pic to Registry",
+    contexts: ["selection"]
+    //contexts: ["image"],
 });
 browser.contextMenus.create ({
-    id: "unregister-dick-pic",
-    title: "Delete Dick Pic from Registry",
-    contexts: ["image"],
+    id      : "unregister-dick-pic",
+    title   : "Delete Dick Pic from Registry",
+    contexts: ["selection"]
+    //contexts: ["image"],
 });
 
 browser.contextMenus.onClicked.addListener (function (info, tab) {
--- content.js
+++ content.js
@@ -3,3 +3,22 @@
 // - check if dick pic
 // - if not dick pic, then do nothing
 // - else replace image with placeholder
+
+// TODO find all image tags
+// TODO replace with loading placeholder
+// TODO on image load, check image, replace as necessary
+// TODO replace placeholders
+
+var imgs = document.images;
+for (var i = 0, iLen = imgs.length; i < iLen; i++) {
+  let img = imgs[i];
+  //img.onload = function () {
+    //let mat = cv.imread (img);
+    //cv.imshow ('canvasOutput', mat);
+    //mat.delete ();
+    // if cv_src is not dick pic: return
+    img.src = 'replacement.png';
+    
+  //  img.onload = function () {}
+  //}
+}
